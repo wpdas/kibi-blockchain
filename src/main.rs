eb88a0d9745ef76ce26218993ffad10e81dc0016
Wenderson Pires <wendersonpdas@gmail.com>
2023-07-07 22:48:52 -0300

    clean main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,11 @@
 // Server - API
 extern crate rocket;
 use rocket::{routes, launch};
-// use rocket::tokio::time::{sleep, Duration};
 
 mod routes;
 
 // Blockchain - Kibi (yes this is my blockchain name)
 mod kibi;
-
-// #[get("/delay/<seconds>")]
-// async fn delay(seconds: u64) -> String {
-//     sleep(Duration::from_secs(seconds)).await;
-//     format!("Waited for {} seconds", seconds)
-// }
 
 #[launch]
 fn rocket() -> _ {
@@ -28,12 +21,4 @@ fn rocket() -> _ {
         .mount("/chain", routes![routes::get_chain::get])
         .mount("/mine", routes![routes::mine_unconfirmed_transactions::get])
         .mount("/create_account", routes![routes::new_account::post])
-        // .mount("/", routes![delay])
 }
-
-// #[rocket::post("/spawn", format = "application/json", data = "<text>")]
-// async fn spawn_endpoint(text: String) -> Result<String, ()> {
-//     run_on_current_thread(123);
-
-//     Ok(text.to_uppercase()) <<<==================== VEJA ISSO E USE COMO EXEMPLO
-// }
